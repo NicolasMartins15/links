6251750c21f59b57948699bb86700b9e34871f5a
Nicolas martins <nicolasdemartins@hotmail.com>
2024-05-20 09:59:33 -0300

    commit 1

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { HomeContainer } from './Home.styles'
+
+export default function Home() {
+  return (
+<HomeContainer>
+  <h1>Home</h1>
+</HomeContainer>
+
+
+  )
+}
